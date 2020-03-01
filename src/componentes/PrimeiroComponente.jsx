bc2d6b90dffdf0db69c0d8753d414e33ed40dfe8
Felipe Marinho <felipmarinho@gmail.com>
2020-02-29 21:08:07 -0300

    First Arrow function component

--- src/componentes/PrimeiroComponente.jsx
+++ src/componentes/PrimeiroComponente.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-function primeiro() {
-	return <h1>Primeiro Componente!</h1>
-}
-
-export default primeiro;
+export default () => (
+	<h1>Primeiro Componente (ARROW)!</h1>
+);
