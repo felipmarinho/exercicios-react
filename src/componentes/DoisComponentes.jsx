efea74a42dbb52d3b9729228e923ec612dd93b93
Felipe Marinho <felipmarinho@gmail.com>
2020-03-02 05:36:26 -0300

    System of modules (import/export)

--- src/componentes/DoisComponentes.jsx
+++ src/componentes/DoisComponentes.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+// export const CompA = props => 
 const CompA = props => 
 	<h1>Primeiro diz: {props.valor}</h1>
 
+// export const CompB = props => 
 const CompB = props => 
 	<h1>Segundo diz: {props.valor}</h1>
 
+// Só posso retornar uma função anonima, pois ela PRECISA ser default.
+// export default function() {
+// 	return <p>teste</p>
+// }
+
 export { CompA, CompB }
+export default CompA
