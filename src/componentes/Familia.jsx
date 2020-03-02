7726a2d255e98ba835b24bde2084522d18981105
Felipe Marinho <felipmarinho@gmail.com>
2020-03-02 06:15:11 -0300

    22 - propagate props element for the children components #2

--- src/componentes/Familia.jsx
+++ src/componentes/Familia.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { filhosComProps } from '../utils/utils'
 
 export default props => 
 	<div>
-		{/* ESSA SOLUÇÃO FUNCIONA APENAS COM UM ELEMENTO*/}
-		<h1>Familia {props.sobrenome}</h1>
-		{/* Criando um clone de props com operador spread {...props}*/} 
-		{React.cloneElement(props.children, {...props})} 
-		{/* {React.cloneElement(props.children, props)} */}
-		{/* {React.cloneElement(props.children,
-			{ sobrenome: props.sobrenome })} */}
+		<h1>Familia</h1>
+		{filhosComProps(props)}
+		
+		{/* Caso não quera isolar essa interação utilize um utils  */}
+		{/* <h2>{props.sobrenome}</h2>
+		{React.Children.map(props.children, filho => {
+			return React.cloneElement(filho, {...props})
+		})}  */}
 	</div>
