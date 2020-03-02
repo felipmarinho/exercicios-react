a084275ff66ca30879c0268e33c7499e5e7fbbb5
Felipe Marinho <felipmarinho@gmail.com>
2020-03-02 04:45:49 -0300

    Using props

--- src/componentes/PrimeiroComponente.jsx
+++ src/componentes/PrimeiroComponente.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export default () => (
-	<h1>Primeiro Componente (ARROW)!</h1>
+let isLegal = true;
+
+export default (props) => (
+	<div>
+		<h1>{props.valor}</h1>
+		<h2>{1 + 1}</h2>
+		<p>Legal? {isLegal ? 'Sim' : 'Não'}</p>
+		<p>{Math.random()}</p>
+	</div>
 );
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import PrimeiroComponente from './componentes/PrimeiroComponente';
 const  elemento = document.getElementById('root');
 ReactDOM.render(
 	<div>
-		<PrimeiroComponente />
+		<PrimeiroComponente valor="Estudos props do component"/>
 	</div>
 	, elemento);
