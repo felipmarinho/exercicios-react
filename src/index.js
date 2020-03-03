42b9bdfd2a08d0e44592ba1b8380386b87e8133c
Felipe Marinho <felipmarinho@gmail.com>
2020-03-03 05:19:40 -0300

    23 - Adding function in a Functional Component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Familia from './componentes/Familia';
-import Membro from './componentes/Membro';
+import ComponenteComFuncao from './componentes/ComponenteComFuncao';
 
 const  elemento = document.getElementById('root');
 ReactDOM.render(
 	<div>
-		<Familia sobrenome='Lima Marinho'>
-			<Membro nome='Felipe' sobrenome='Marinho' />
-			<Membro nome='Viviane' sobrenome='Lima' />
-		</Familia>
+		<ComponenteComFuncao />
 	</div>
 	, elemento);
