973f205e222e99fdaf06110a604d5e149acb696a
Felipe Marinho <felipmarinho@gmail.com>
2020-03-09 06:03:37 -0300

    29 - Using 'state' in class component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import Contador from './componentes/Contador';
 const  elemento = document.getElementById('root');
 ReactDOM.render(
 	<div>
-		<Contador numero={0}/>
+		<Contador numeroInicial={100}/>
 	</div>
 	, elemento);
